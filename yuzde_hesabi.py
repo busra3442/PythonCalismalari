0fbbf7aaeddf143a35d0c49ac21b6306d5600b8c
Busra Sevindik <busrasevindik182@gmail.com>
2024-08-21 10:55:54 +0300

    initial commit

---
+++ yuzde_hesabi.py
@@ -0,0 +1,6 @@
+a = int(input("Hangi sayının yüzdesini almak istersin? "))
+b = int(input("Bu sayının yüzde kaçını alak istersin? "))
+
+c = a * b/100
+
+print(f"{a} sayısının yüzde {b} si = {c}") #baştaki f işlem olmasını sağlıyor
